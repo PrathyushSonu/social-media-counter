5cf92ce40cf407e3efd988eaa9119ed68fca0934
Prathyush NP <158449820+PrathyushSonu@users.noreply.github.com>
2025-04-21 18:45:17 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SocialMediaCounter from "./SocialMediaCounter";
+
+function App() {
+  return (
+    <div>
+      <SocialMediaCounter />
+    </div>
+  );
+}
+
+export default App;
